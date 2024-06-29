2297361e1855d06c65e9cc3f3a27d85d9d6ef375
Pavel V <pavel@299792458.ru>
2024-06-29 19:31:50 +0300

    qbtlib

---
+++ bin/qbtlib.sh
@@ -0,0 +1,33 @@
+# usage:
+# bash qbtlib.sh last | grep Отечественная | cut -f1 | tail | bash qbtlib.sh resume
+
+tmp=/tmp/qbtlib.sh.cache_$(date +%F_%R).zst
+
+_apicall() {
+	s=--silent
+	#s=--verbose
+	set -vx
+	curl $s \
+		-G http://${QBT_HOST:-localhost:8283}/api/v2/torrents/$1 \
+		"${@:2}"
+	set +vx
+}
+
+case $1 in
+cache)
+	cat $(ls -1t /tmp/qbtlib.sh.cache_* | head -n1) | zstdmt -d
+	;;
+last)
+	_apicall info -G --data "sort=added_on" | \
+		jq -r '.[] | [ .hash, .category, .content_path ] | @tsv' | \
+		zstdmt --adapt | tee $tmp | zstdmt -d
+	;;
+resume)
+	hashes=$(paste -sd\|)
+	_apicall resume -X POST --data "hashes=$hashes"
+	;;
+recheck)
+	hashes=$(paste -sd\|)
+	_apicall recheck -X POST --data "hashes=$hashes"
+	;;
+esac
